a734ba7a204999aee739fb4b1f6e805394e6501e
Edson Carvalho <edson.carvalho@ubook.com>
2021-10-29 17:49:08 -0300

    test my GA in js file

--- js/tagueamento.js
+++ js/tagueamento.js
@@ -16,7 +16,15 @@
 //   gtag('config', 'G-XVDK82KPSF');
 // </script>
 
+(function (i, s, o, g, r, a, m) {
+i['GoogleAnalyticsObject'] = r; i[r] = i[r] || function () {
+    (i[r].q = i[r].q || []).push(arguments)
+}, i[r].l = 1 * new Date(); a = s.createElement(o),
+    m = s.getElementsByTagName(o)[0]; a.async = 1; a.src = g; m.parentNode.insertBefore(a, m)
+})(window, document, 'script', 'https://www.googletagmanager.com/gtag/js?id=G-XVDK82KPSF', 'ga');
 
+ga('config', 'G-XVDK82KPSF');
+ga('send', 'pageview');
 
 // (function (i, s, o, g, r, a, m) {
 // i['GoogleAnalyticsObject'] = r; i[r] = i[r] || function () {
