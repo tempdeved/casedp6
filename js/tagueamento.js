88eb9d09e1a45091df1923eb214a80b1373bade5
Edson Carvalho <edson.carvalho@ubook.com>
2021-10-31 22:09:15 -0300

    removing my credentials and ending the test

--- js/tagueamento.js
+++ js/tagueamento.js
@@ -5,11 +5,20 @@
 // para fazer a sua coleta.
 // Caso tenha alguma dúvida sobre o case, não hesite em entrar em contato
 
-function tag_link_externo() {
-    ga('send', 'event', 'menu', 'entre_em_contato', 'link_externo');
-}
-function tag_download_pdf() {
-    ga('send', 'event', 'menu', 'download_pdf', 'download_pdf');
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+ga('create', 'UA-12345-6', 'auto');
+ga('send', 'pageview');
+
+ var captureOutboundLink = function(url, acao, rotulo) {
+    ga('send', 'event', 'menu', acao, rotulo, {
+      'transport': 'beacon',
+      'hitCallback': function(){document.location = url;}
+    });
+    console.log(url )
  }
 
 function tag_Lorem() {
